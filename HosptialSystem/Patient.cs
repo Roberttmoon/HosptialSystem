9b0d573276c6d0f7f8597c90462d5eef20ee7113
U-DESKTOP-FG7TI8O\rotomoMBPwin <Roberttmoon@gmail.com>
2015-12-01 15:15:16 -0600

    adding Patient file

---
+++ HosptialSystem/Patient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hosptial
+{
+    public class Patient
+    {
+        //patent information
+        string patientID;
+        string patientLastName;
+        string patientFirstName;
+        string patientBirthDate;
+        string patientPhoneNumber;
+        string patientPrimaryDoctor;
+
+
+        //patient history
+        List<List<patientCurrentSymptoms>> patientPastSymptoms;
+        List<string> patentAllergys;
+        List<string> patentPastSurgerys;
+
+        //appointment details
+        DateTime patientApointmentDateTime;
+        string patientApointmentRoom;
+        string patientApointmentDoctor;
+        List<string> patientCurrentSymptoms;
+
+        //insurance details
+        bool hasInsurance;
+        string insuranceCompanyName;
+        string memberID;
+        string groupID;
+        string insuranceExpirationDate;
+
+    }
+}
