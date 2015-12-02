51c03987290745a638b3e49e9d4011a1af380f49
mcnuggz <dzanfox@gmail.com>
2015-12-01 18:05:33 -0600

    deleted halp

--- HosptialSystem/Appointment.cs
+++ HosptialSystem/Appointment.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HosptialSystem
