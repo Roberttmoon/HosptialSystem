08b7897ca90568c5fdbd1b2fd57f40f14f3f6b38
roberttmoon@gmail.com <roberttmoon@gmail.com>
2015-12-04 16:07:27 -0600

    added json file reading

--- HosptialSystem/InputOutput.cs
+++ HosptialSystem/InputOutput.cs
@@ -1,16 +1,32 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace HosptialSystem
 {
     class InputOutput
     {
+        public void writeToXML(Person incomingObject)
+        {
+            var filePath = "C:\\Users\\Robert Moon\\Documents\\Class\\GitHub\\HosptialSystem\\HosptialSystem\\PeopleDatabase.json";
+            var jsonFile = System.IO.File.ReadAllText(filePath);
+            Console.WriteLine(jsonFile);
+            var peopleDataList = JsonConvert.DeserializeObject<List<Person>>(jsonFile) ?? new List<Person>();
+            peopleDataList.Add(incomingObject);
 
+            jsonFile = JsonConvert.SerializeObject(peopleDataList);
+            System.IO.File.WriteAllText(filePath, jsonFile);
+        }
+        public void readFromXML()
+        {
 
-
-
+        }
+      
     }
 }
